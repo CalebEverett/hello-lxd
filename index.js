985d3b88b373f94c5c41725954d70ed46182828e
Caleb Everett <caleb@careercalifornia.edu>
2016-06-10 13:00:14 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8080;
 require('http').createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('hello lxd');
