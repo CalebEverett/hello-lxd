97eefcf94c2fa7f527227bf33f14e0890b9cb89e
root <root@mycontainer.lxd>
2016-06-10 19:58:25 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+var port = process.env.PORT || 8080
 require('http').createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('hello lxd');
   res.end();
-}).listen(8080);
+}).listen(port);
 
-console.log('Listenting on port 8080');
+console.log('Listenting on port ' + port);
